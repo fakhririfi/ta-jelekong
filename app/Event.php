f314393b998999170a064c8f8d2b099b6e7c64dc
madedarmawijaya <madedarma9000@gmail.com>
2021-06-25 13:30:16 +0800

    add: model dan migration event

---
+++ app/Event.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $table = "events";
+
+    protected $fillable = [
+        'name',
+        'time',
+        'location',
+        'description',
+        'price',
+        'quota',
+        'image'
+    ];
+}
