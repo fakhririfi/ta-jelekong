4f538c4854059542dc76261e8581ac654d2a438e
madedarmawijaya <madedarma9000@gmail.com>
2021-06-25 13:35:44 +0800

    add: index event (event controller)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -15,6 +15,13 @@ class EventController extends Controller
     public function index()
     {
         //
+        $events = Event::all();
+
+        return view('admin.events.index')->with([
+            'events' => $events
+        ]);
+
+
     }
 
     /**
