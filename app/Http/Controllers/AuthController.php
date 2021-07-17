eb7e0b80de2f3a6f72f45387f93bf58aa2991669
fakhririfqifirdaus <fakhririfi@student.telkomuniversity.ac.id>
2021-07-17 20:15:54 +0700

    add AuthController-chng regis sys

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+
+
+class AuthController extends Controller
+{
+
+    /**
+     * Validate and create a newly registered user.
+     *
+     * @param  array  $input
+     * @return \App\Models\User
+     */
+
+    public function registerView()
+    {
+        return view('auth.register');
+    }
+
+    public function create(Request $request)
+    {
+        $rules = [
+            'name'                  => 'required|string|min:3|max:255',
+            'last_name'             => 'required|string|min:3|max:255',
+            'email'                 => 'required|email|string|max:255|unique:users,email',
+            'password'              => 'required|confirmed'
+        ];
+
+        $messages = [
+            'name.required'         => 'Nama Lengkap wajib diisi',
+            'name.min'              => 'Nama lengkap minimal 3 karakter',
+            'name.max'              => 'Nama lengkap maksimal 255 karakter',
+            'email.required'        => 'Email wajib diisi',
+            'email.email'           => 'Email tidak valid',
+            'email.unique'          => 'Email sudah terdaftar',
+            'password.required'     => 'Password wajib diisi',
+            'password.confirmed'    => 'Password tidak sama dengan konfirmasi password'
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput($request->all);
+        }
+
+        $user = new User;
+        $user->name = ucwords(strtolower($request->name));
+        $user->last_name = ucwords(strtolower($request->last_name));
+        $user->email = strtolower($request->email);
+        $user->email_verified_at = \Carbon\Carbon::now();
+        $user->password = Hash::make($request->password);
+        $simpan = $user->save();
+
+        if($simpan){
+            Session::flash('success', 'Register berhasil! Silahkan login untuk mengakses data');
+            return redirect()->route('login');
+        } else {
+            Session::flash('errors', ['' => 'Register gagal! Silahkan ulangi beberapa saat lagi']);
+            return redirect()->route('register');
+        }
+    }
+}
