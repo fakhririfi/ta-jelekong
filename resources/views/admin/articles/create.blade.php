c098479ff1394824cd42b982a553d610143591c8
Aditya312 <fernandaadityan83@gmail.com>
2021-07-27 00:38:18 +0700

    create and index article

---
+++ resources/views/admin/articles/create.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.admin')
+
+@section('main-content')
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800">{{ __('Buat Article') }}</h1>
+
+<!-- Main Content goes here -->
+
+@if ($errors->any())
+<div class="alert alert-danger border-left-danger" role="alert">
+    <ul class="pl-4 my-2">
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
+<form action="{{ route('articles.store') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    <div class="mb-3 row">
+        <label class="col-sm-2 col-form-label">Judul Article</label>
+        <div class="col-sm-10">
+            <input name="title" type="text" class="form-control">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label class="col-sm-2 col-form-label">Content</label>
+        <div class="col-sm-10">
+            <textarea name="content" class="form-control ckeditor"></textarea>
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <label class="col-sm-2 col-form-label">Upload</label>
+        <div class="col-sm-10">
+            <input name="image" type="file" class="form-control-plaintext">
+        </div>
+    </div>
+    <div class="mb-3 row">
+        <button class="btn btn-primary mx-auto w-25">Simpan</button>
+    </div>
+</form>
+
+<!-- End of Main Content -->
+@endsection
+
+@push('notif')
+@if (session('success'))
+<div class="alert alert-success border-left-success alert-dismissible fade show" role="alert">
+    {{ session('success') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
+
+@if (session('status'))
+<div class="alert alert-success border-left-success" role="alert">
+    {{ session('status') }}
+</div>
+@endif
+@endpush
+
+@push('js')
+<script src="https://cdn.ckeditor.com/ckeditor5/29.0.0/classic/ckeditor.js"></script>
+<script>
+    ClassicEditor
+        .create(document.querySelector('.ckeditor'))
+        .catch(error => {
+            console.error(error);
+        });
+</script>
+@endpush 
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,22 +15,18 @@
     <!-- Fonts -->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
-
     <!-- Favicon -->
     <link href="{{ asset('img/favicon.png') }}" rel="icon" type="image/png">
-
     @stack('css')
 </head>
-<body id="page-top">
 
+<body id="page-top">
 <!-- Page Wrapper -->
 <div id="wrapper">
     <!-- Sidebar -->
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
         <!-- Sidebar - Brand -->
         <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
             <div class="sidebar-brand-icon rotate-n-15">
@@ -38,10 +34,8 @@
             </div>
             <div class="sidebar-brand-text mx-3">Jelekong</div>
         </a>
-
         <!-- Divider -->
         <hr class="sidebar-divider my-0">
-
         <!-- Nav Item - Profile -->
         @if(Auth::check())
         <li class="nav-item {{ Nav::isRoute('events.*') }}">
@@ -53,6 +47,18 @@
                 <div class="bg-white py-2 collapse-inner rounded">
                     <a class="collapse-item" href="{{ route('events.create') }}">Buat Event</a>
                     <a class="collapse-item" href="{{ route('events.index') }}">List Event</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item {{ Nav::isRoute('articles.*') }}">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseArticle" aria-expanded="true" aria-controls="collapseArticle">
+                <i class="fas fa-fw fa-book"></i>
+                <span>Articles</span>
+            </a>
+            <div id="collapseArticle" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <a class="collapse-item" href="{{ route('articles.create') }}">Buat Article</a>
+                    <a class="collapse-item" href="{{ route('articles.index') }}">List Article</a>
                 </div>
             </div>
         </li>
@@ -69,7 +75,6 @@
             </div>
         </li>
         @endif
-
         <!-- <li class="nav-item {{ Nav::isRoute('profile') }}">
             <a class="nav-link" href="{{ route('profile') }}">
                 <i class="fas fa-fw fa-user"></i>
@@ -88,38 +93,28 @@
                 <span>{{ __('Blank Page') }}</span>
             </a>
         </li> -->
-
         <!-- Divider -->
         <hr class="sidebar-divider d-none d-md-block">
-
         <!-- Sidebar Toggler (Sidebar) -->
         <div class="text-center d-none d-md-inline">
             <button class="rounded-circle border-0" id="sidebarToggle"></button>
         </div>
-
     </ul>
     <!-- End of Sidebar -->
-
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
-
         <!-- Main Content -->
         <div id="content">
-
             <!-- Topbar -->
             <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
                 <!-- Sidebar Toggle (Topbar) -->
                 <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
                     <i class="fa fa-bars"></i>
                 </button>
-
                 <!-- Topbar Navbar -->
                 @if(Auth::check())
                 <ul class="navbar-nav ml-auto">
-
                     <div class="topbar-divider d-none d-sm-block"></div>
-
                     <!-- Nav Item - User Information -->
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -139,23 +134,18 @@
                             </a>
                         </div>
                     </li>
-
                 </ul>
                 @endif                    
             </nav>
             <!-- End of Topbar -->
-
             <!-- Begin Page Content -->
             <div class="container-fluid">
                 @stack('notif')
                 @yield('main-content')
-
             </div>
             <!-- /.container-fluid -->
-
         </div>
         <!-- End of Main Content -->
-
         <!-- Footer -->
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
@@ -165,17 +155,13 @@
             </div>
         </footer>
         <!-- End of Footer -->
-
     </div>
     <!-- End of Content Wrapper -->
-
 </div>
-
 <!-- Scroll to Top Button-->
 <a class="scroll-to-top rounded" href="#page-top">
     <i class="fas fa-angle-up"></i>
 </a>
-
 @if(Auth::check())
 <!-- Logout Modal-->
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -199,7 +185,6 @@
     </div>
 </div>
 @endif
-
 <!-- Scripts -->
 <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
 <script src="{{ asset('vendor/bootstrap/js/bootstrap.min.js') }}"></script>
