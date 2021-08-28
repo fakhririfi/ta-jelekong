89395e665a73dd129ff1c72fac1ab46aeb35d91b
madedarmawijaya <madedarma9000@gmail.com>
2021-08-28 18:52:16 +0800

    fix: db organizer

--- database/migrations/2021_06_23_134405_add_organizer_to_events_table.php
+++ database/migrations/2021_06_23_134405_add_organizer_to_events_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddAdministratorToEventsTable extends Migration
+class AddOrganizerToEventsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,7 @@ class AddAdministratorToEventsTable extends Migration
     public function up()
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->integer('administrator_count');
-            $table->text('administrators');
+            $table->string('organizer')->after('quota');
         });
     }
 
@@ -27,7 +26,7 @@ class AddAdministratorToEventsTable extends Migration
     public function down()
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->dropColumn(['administrator_count', 'administrators']);
+            $table->dropColumn("organizer");
         });
     }
 }
